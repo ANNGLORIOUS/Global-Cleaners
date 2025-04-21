24b64d60e01dae782b1debec3bc10be4aabb0c7e
ANNGLORIOUS <mueniannglorious@gmail.com>
2025-04-21 12:15:24 +0300

    added some fonts and colors

--- global-cleaning/vite.config.js
+++ global-cleaning/vite.config.js
@@ -2,6 +2,27 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", 
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        libre: ['"Libre Baskerville"', 'serif'],
+        bai: ['"Bai Jamjuree"', 'sans-serif'],
+      },
+      colors: {
+        primary: "#0A3D2E",       // Deep Emerald
+        accent: "#D4AF37",        // Warm Gold
+        background: "#F5FCFF",    // Light Blue-White
+        card: "#FFFFFF",          // Clean White
+        muted: "#e2e8f0",         // Soft Gray
+        darktext: "#2E2E2E",      // Deep Charcoal
+        success: "#10B981",       // Emerald Green for Buttons
+      },
+    },
+  },
   plugins: [
     tailwindcss(),
   ],
