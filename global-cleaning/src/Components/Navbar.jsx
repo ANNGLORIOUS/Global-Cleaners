2943ea35267c1dcd507f9e73139bf17521ab03a8
ANNGLORIOUS <mueniannglorious@gmail.com>
2025-04-22 19:26:13 +0300

    added some color to the footer

--- global-cleaning/src/Components/Navbar.jsx
+++ global-cleaning/src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-primary text-white shadow-md">
+    <nav className="bg-primary text-black shadow-md">
       <div className="container mx-auto px-4 py-3">
         <div className="flex justify-between items-center">
           {/* Logo */}
